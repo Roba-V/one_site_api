abed4c7168c9d67dba1be5b7ac7dff5b035b052a
Roba <roba@vrstu.net>
2022-07-16 00:03:00 +0900

    管理サイト設定変更
    - 言語管理クラス

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Language
 
-# Register your models here.
+
+class LanguageAdmin(admin.ModelAdmin):
+    """言語管理クラス"""
+
+    # 一覧画面に表示するフィールド
+    list_display = ("name", "code")
+
+    # リンクを貼り付ける一覧画面のフィールド
+    list_display_links = ("name", "code")
+
+    # 編集画面で表示するフィールド
+    fieldsets = (
+        (None, {"fields": ("name", "code", ("created_user", "updated_user"))}),
+        (
+            "オプション",
+            {
+                "classes": ("collapse",),
+                "fields": ("alias", "sort"),
+            },
+        ),
+    )
+
+
+admin.site.register(Language, LanguageAdmin)
