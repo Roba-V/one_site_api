f27f5d2468d61548bfb33beb55db0f2485cc8845
Roba <roba@vrstu.net>
2022-07-17 18:18:28 +0900

    管理サイト設定変更
    - カテゴリー管理クラス

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
-from .models import Language, Tag
+from .models import Category, Language, Tag
 
 
 class LanguageAdmin(admin.ModelAdmin):
     """言語管理クラス"""
 
-    # 一覧画面に表示するフィールド
-    list_display = ("name", "code")
-
-    # リンクを貼り付ける一覧画面のフィールド
-    list_display_links = ("name", "code")
+    # 一覧画面に表示するフィールド & リンクを貼り付ける一覧画面のフィールド
+    list_display = list_display_links = ("name", "code")
 
     # 編集画面で表示するフィールド
     fieldsets = (
@@ -27,11 +24,8 @@ class LanguageAdmin(admin.ModelAdmin):
 class TagAdmin(admin.ModelAdmin):
     """タグ管理クラス"""
 
-    # 一覧画面に表示するフィールド
-    list_display = ("name", "language")
-
-    # リンクを貼り付ける一覧画面のフィールド
-    list_display_links = ("name", "language")
+    # 一覧画面に表示するフィールド & リンクを貼り付ける一覧画面のフィールド
+    list_display = list_display_links = ("name", "language")
 
     # リストフィルター
     list_filter = ("language",)
@@ -59,5 +53,53 @@ class TagAdmin(admin.ModelAdmin):
     )
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    """カテゴリー管理クラス"""
+
+    # 一覧画面に表示するフィールド & リンクを貼り付ける一覧画面のフィールド
+    list_display = list_display_links = (
+        "name",
+        "category_type",
+        "parent_category",
+        "language",
+    )
+
+    # リストフィルター
+    list_filter = ("category_type", "language")
+
+    # 検索フィルター
+    search_fields = ("name",)
+
+    # 編集画面で表示するフィールド
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "name",
+                    ("category_type", "parent_category"),
+                    ("created_user", "updated_user"),
+                    "language",
+                )
+            },
+        ),
+        (
+            "オプション",
+            {
+                "classes": ("collapse",),
+                "fields": ("sort", "alias"),
+            },
+        ),
+        (
+            "SEO",
+            {
+                "classes": ("collapse",),
+                "fields": ("description", "keywords"),
+            },
+        ),
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Language, LanguageAdmin)
 admin.site.register(Tag, TagAdmin)
