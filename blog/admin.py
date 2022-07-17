9583cf2ee67807ceddbb5a5a8fc6d8dee66c6bb5
Roba <roba@vrstu.net>
2022-07-17 20:50:37 +0900

    文章管理クラスを追加

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Language, Tag
+from .models import Article, Category, Language, Tag
 
 
 class LanguageAdmin(admin.ModelAdmin):
@@ -35,7 +35,13 @@ class TagAdmin(admin.ModelAdmin):
 
     # 編集画面で表示するフィールド
     fieldsets = (
-        (None, {"fields": ("name", ("created_user", "updated_user"), "language")}),
+        (None, {"fields": ("name", "language")}),
+        (
+            "作成者情報",
+            {
+                "fields": (("created_user", "updated_user"),),
+            },
+        ),
         (
             "オプション",
             {
@@ -100,6 +106,62 @@ class CategoryAdmin(admin.ModelAdmin):
     )
 
 
-admin.site.register(Category, CategoryAdmin)
+class ArticleAdmin(admin.ModelAdmin):
+    """文章管理クラス"""
+
+    # 一覧画面に表示するフィールド & リンクを貼り付ける一覧画面のフィールド
+    list_display = list_display_links = (
+        "title",
+        "category",
+        "language",
+        "published_at",
+    )
+
+    # リストフィルター
+    list_filter = ("category", "language")
+
+    # 検索フィルター
+    search_fields = ("title",)
+
+    # 日付
+    date_hierarchy = "published_at"
+
+    # 編集画面で表示するフィールド
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "title",
+                    "overview",
+                    "content",
+                    "file",
+                    "category",
+                    "tags",
+                    "published_at",
+                    ("created_user", "updated_user"),
+                    "language",
+                )
+            },
+        ),
+        (
+            "オプション",
+            {
+                "classes": ("collapse",),
+                "fields": ("sort", "alias"),
+            },
+        ),
+        (
+            "SEO",
+            {
+                "classes": ("collapse",),
+                "fields": ("description", "keywords"),
+            },
+        ),
+    )
+
+
 admin.site.register(Language, LanguageAdmin)
 admin.site.register(Tag, TagAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
